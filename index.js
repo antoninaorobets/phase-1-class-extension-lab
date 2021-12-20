c6d7c026232cb2f4b62bd6a825ea580226225ffa
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-20 12:40:12 -0500

    commit tree cc7ff30629143b0b72d55675171926cd64639659

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Polygon {
+    constructor (arr) {
+        this.arr =arr
+    }
+    get countSides() {
+        return this.arr.length
+    }
+    get perimeter() {
+        let per = 0
+        for (let side of this.arr) {
+            per = per + side
+        }
+        return per
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        if (this.arr[0]< (this.arr[1]+this.arr[2]) && this.arr[1]< (this.arr[0]+this.arr[2]) && this.arr[2]< (this.arr[1]+this.arr[0])  ) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        for (let i=0; i<3;i++) {
+            if (this.arr[i] !== this.arr[i+1]) {return false }
+        return true 
+    }}
+    get area() {
+        return this.arr[0] * this.arr[0]
+    }
+}
